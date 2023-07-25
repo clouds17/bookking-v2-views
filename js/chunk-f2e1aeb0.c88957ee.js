88985781f0e694f571a12f0484c2a932a7b75da0
黄鑫 <990695619@qq.com>
2023-07-25 23:34:36 +0800

    deploy

---
+++ js/chunk-f2e1aeb0.c88957ee.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-f2e1aeb0"],{"74c4":function(t,c,n){"use strict";n("f7bc")},9703:function(t,c,n){"use strict";n.r(c);var e=function(){var t=this,c=t.$createElement,n=t._self._c||c;return n("div",[n("p",{staticClass:"not-found"},[t._v("当前页面不存在，请检查网址是否正确")]),n("router-link",{staticClass:"backHome",attrs:{to:"/"}},[t._v("返回首页")])],1)},s=[],a={},o=a,u=(n("74c4"),n("2877")),i=Object(u["a"])(o,e,s,!1,null,"91cd59ac",null);c["default"]=i.exports},f7bc:function(t,c,n){}}]);
+//# sourceMappingURL=chunk-f2e1aeb0.c88957ee.js.map
---
+++ precache-manifest.a18eb70c819fcba3b758d4a4972c2fad.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "117d53b2947181272066",
+    "url": "/bookking-v2-views/css/app.16dee665.css"
+  },
+  {
+    "revision": "2f2e782069682fd43abe",
+    "url": "/bookking-v2-views/css/chunk-04852ead.8532eef2.css"
+  },
+  {
+    "revision": "da985f2c6f510f63abc0",
+    "url": "/bookking-v2-views/css/chunk-15c93940.d61e97de.css"
+  },
+  {
+    "revision": "5fd190610817400a803e",
+    "url": "/bookking-v2-views/css/chunk-53b2a55b.4d9f3475.css"
+  },
+  {
+    "revision": "b293b8ed0e89d4e696d1",
+    "url": "/bookking-v2-views/css/chunk-577b08d7.eb1b5066.css"
+  },
+  {
+    "revision": "2708f5861e0a6781a56a",
+    "url": "/bookking-v2-views/css/chunk-f2e1aeb0.56d3a646.css"
+  },
+  {
+    "revision": "e922b633e8516a923453",
+    "url": "/bookking-v2-views/css/chunk-f54e7638.c3679dad.css"
+  },
+  {
+    "revision": "798ffa814b7ef3eb0f40ed3d170abaf1",
+    "url": "/bookking-v2-views/index.html"
+  },
+  {
+    "revision": "117d53b2947181272066",
+    "url": "/bookking-v2-views/js/app.cd14e5c7.js"
+  },
+  {
+    "revision": "2f2e782069682fd43abe",
+    "url": "/bookking-v2-views/js/chunk-04852ead.d2068c0f.js"
+  },
+  {
+    "revision": "da985f2c6f510f63abc0",
+    "url": "/bookking-v2-views/js/chunk-15c93940.c9367d49.js"
+  },
+  {
+    "revision": "afa33eee2bc368d97587",
+    "url": "/bookking-v2-views/js/chunk-4d3092d4.5ce717cd.js"
+  },
+  {
+    "revision": "5fd190610817400a803e",
+    "url": "/bookking-v2-views/js/chunk-53b2a55b.ffa772ee.js"
+  },
+  {
+    "revision": "b293b8ed0e89d4e696d1",
+    "url": "/bookking-v2-views/js/chunk-577b08d7.046a7fda.js"
+  },
+  {
+    "revision": "2708f5861e0a6781a56a",
+    "url": "/bookking-v2-views/js/chunk-f2e1aeb0.c88957ee.js"
+  },
+  {
+    "revision": "e922b633e8516a923453",
+    "url": "/bookking-v2-views/js/chunk-f54e7638.0d989820.js"
+  },
+  {
+    "revision": "3f9395035e3b4c89754a",
+    "url": "/bookking-v2-views/js/chunk-vendors.5fe766b3.js"
+  },
+  {
+    "revision": "77b40b736ba7ed8a0f02154dff731a04",
+    "url": "/bookking-v2-views/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/bookking-v2-views/robots.txt"
+  }
+]);
